b58ad2e3244d3f012ae517b02266f50dc6e99888
presh-031 <akingbulugbe.dev@gmail.com>
2023-02-05 13:40:48 +0100

    now getting data for individual product page

--- pages/headphones/[id].js
+++ pages/headphones/[id].js
@@ -1,7 +1,35 @@
-import React from "react";
+import { products } from "../../data";
 
-const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+export const getStaticPaths = () => {
+  const paths = products
+    .filter((product) => {
+      return product.category === "headphones";
+    })
+    .map((headphone) => {
+      return {
+        params: { id: headphone.slug },
+      };
+    });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const getStaticProps = (context) => {
+  const id = context.params.id;
+
+  return {
+    props: {
+      headphone: products.filter((product) => {
+        return product.slug === id;
+      }),
+    },
+  };
+};
+
+const ProductDetail = ({ headphone }) => {
+  console.log(headphone);
+  return <div></div>;
 };
 
 export default ProductDetail;
