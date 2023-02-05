9251fb0c1dae1f002965badb38afa86617cd9892
presh-031 <akingbulugbe.dev@gmail.com>
2023-02-05 07:00:55 +0100

    sill working on product images

--- components/ProductItem.jsx
+++ components/ProductItem.jsx
@@ -7,7 +7,7 @@ const ProductItem = ({ item }) => {
   return (
     <div className="outline outline-red-800">
       <div>
-        <Image />
+        <Image src={require("." + "./." + item?.image.mobile)} alt="product-image" height={352} width={327} />
       </div>
       <div>
         {item.new && <p>new product</p>}
