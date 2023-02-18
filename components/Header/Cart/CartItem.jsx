e65ce63a49e7baf5c6d16347de243d34c4c25a9c
user <user@users-MacBook-Pro.local>
2023-02-17 17:19:07 -0800

    now showing cart items

--- components/Header/Cart/CartItem.jsx
+++ components/Header/Cart/CartItem.jsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 const CartItem = ({ id, image, name, price, quantity }) => {
   console.log(id, image, name, price, quantity);
   return (
-    <div>
+    <div className="flex items-center gap-[1.6rem]">
       <div>
-        <Image />
+        <Image className="outline" alt="" width={64} height={64} />
       </div>
-      <div>
-        <p>{name}</p>
-        <p>${price}</p>
+      <div className="flex flex-col  border-red-800 font-bold leading-[2.5rem]">
+        <p className="w-[8rem] truncate text-[1.5rem] ">{name}</p>
+        <p className="text-[1.4rem] font-medium opacity-50  ">${price}</p>
       </div>
-      <div className="flex items-center bg-[#f1f1f1] py-[0.5rem] text-[1.3rem] font-bold leading-[1.776rem]  tracking-[1px] ">
+      <div className="flex h-[3.2rem] items-center bg-[#f1f1f1] py-[0.5rem] text-[1.3rem] font-bold leading-[1.776rem]  tracking-[1px] ">
         <button className="px-[1.5rem] py-[1rem]">-</button>
         <p className="px-[0.5rem]">{quantity}</p>
         <button className="px-[1.5rem] py-[1rem]">+</button>
--- components/Header/Cart/index.jsx
+++ components/Header/Cart/index.jsx
@@ -7,12 +7,14 @@ const Cart = () => {
   const cart = useSelector((state) => state.cart);
   // console.log(cart);
   return (
-    <div className="absolute right-[25px] top-[90px] left-[25px] outline outline-red-800">
-      <div>
-        <p>
+    <div className="absolute right-[25px] top-[90px] left-[25px] bg-white px-[2.8rem] py-[3.2rem] outline outline-red-800">
+      <div className="mb-[3.1rem] flex justify-between">
+        <p className="text-[1.8rem] font-bold leading-[2.4590rem] tracking-[.129rem]">
           CART <span>({})</span>
         </p>
-        <p>Remove all</p>
+        <p className="text-[1.5rem] font-medium leading-[2.5rem] text-black opacity-50 hover:underline">
+          Remove all
+        </p>
       </div>
       {cart?.map((item) => (
         <CartItem
