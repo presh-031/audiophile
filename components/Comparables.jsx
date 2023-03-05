4f30761cfde6e8f86220fdc89945fa29aaad1d06
presh-031 <akingbulugbe.dev@gmail.com>
2023-03-05 20:19:53 +0100

    fix: still working on comparable

--- components/Comparables.jsx
+++ components/Comparables.jsx
@@ -5,15 +5,17 @@ const Comparables = ({ comparables }) => {
       <p className="mb-[4rem] text-center text-[2.4rem] font-bold leading-[3.6rem] tracking-[0.086rem] outline">
         YOU MAY ALSO LIKE
       </p>
-      {comparables.map((comparable) => {
+      {comparables.map((comparable) => (
         <div>
-          <div className="mt-[4rem]  mb-[3.2rem] h-[12rem]  ">
+          <div className="mt-[4rem] mb-[3.2rem]  h-[12rem] outline  ">
             {/* <Image /> */}
           </div>
-          <p className="mb-[3.2rem]"></p>
-          {/* <ButtonOne url={""} /> */}
-        </div>;
-      })}
+          <p className="mb-[3.2rem] text-center text-[2.4rem] font-bold leading-[3.278rem] tracking-[.171rem]">
+            {comparable.name}
+          </p>
+          {/* <ButtonOne url={`//${comparable.slug}`} /> */}
+        </div>
+      ))}
     </div>
   );
 };
