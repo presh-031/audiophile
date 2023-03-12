0eb17bd6005bc665c62e501fae3d9d9b581f116a
presh-031 <akingbulugbe.dev@gmail.com>
2023-03-12 23:37:07 +0100

    fix: fixed broken images at comparables component

--- components/AllProducts/ProductCategory.jsx
+++ components/AllProducts/ProductCategory.jsx
@@ -6,7 +6,7 @@ const ProductCategory = ({ categoryImg, categoryTitle }) => {
   return (
     <div className=" mt-[6.8rem] flex flex-col items-center bg-[#f1f1f1] text-center">
       <Image
-        className="relative top-[-5.2rem] mb-[-5.2rem]  "
+        className="relative top-[-5.2rem] mb-[-5.2rem]"
         src={categoryImg}
         alt="category"
         height={133}
--- components/Comparables.jsx
+++ components/Comparables.jsx
@@ -5,21 +5,23 @@ const Comparables = ({ comparables }) => {
   console.log(comparables);
   console.log(comparables[0].image.mobile.slice(1));
   return (
-    <div className="">
-      <p className="mb-[4rem] text-center text-[2.4rem] font-bold leading-[3.6rem] tracking-[0.086rem] outline">
+    <div className="mb-[17.2rem] ">
+      <p className="mb-[4rem] text-center text-[2.4rem] font-bold leading-[3.6rem] tracking-[0.086rem] ">
         YOU MAY ALSO LIKE
       </p>
       {comparables.map((comparable) => {
         const image = comparable.image.mobile;
         return (
-          <div key={comparable.slug}>
+          <div
+            key={comparable.slug}
+            className="mb-[5.6rem] flex flex-col items-center "
+          >
             <Image
-              // src={image.slice(1)}
-              src="/assets/shared/mobile/image-xx99-mark-two-headphones.jpg"
+              src={image.slice(1)}
               alt="product"
               width={372}
               height={120}
-              className="mt-[4rem] mb-[3.2rem]"
+              className="mb-[3.2rem]"
             />
 
             <p className="mb-[3.2rem] text-center text-[2.4rem] font-bold leading-[3.278rem] tracking-[.171rem]">
