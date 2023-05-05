afeb9ba3d3b94c9f2860f70eb81557809e619e87
presh-031 <akingbulugbe.dev@gmail.com>
2023-05-06 00:04:43 +0100

    fix: used proper jsx label attribute

--- pages/checkout.js
+++ pages/checkout.js
@@ -10,28 +10,28 @@ const checkout = () => {
         <form>
           <p>BILLING DETAILS</p>
           <div>
-            <label for="name">Name</label>
+            <label htmlFor="name">Name</label>
             <input type="text" placeholder="Alexel Ward" id="name" />
 
-            <label for="email">Email Address</label>
+            <label htmlFor="email">Email Address</label>
             <input type="text" placeholder="alexel@mail.com" id="email" />
 
-            <label for="phone">Phone Number</label>
+            <label htmlFor="phone">Phone Number</label>
             <input type="phone" placeholder="+1 202-555-0136" id="phone" />
           </div>
 
           <p>SHIPPING INFO</p>
-          <label for="address">Your Address</label>
+          <label htmlFor="address">Your Address</label>
           <input type="text" placeholder="1137 Williams Avenue" id="address" />
 
           <div>
-            <label for="zip-code">Email Address</label>
+            <label htmlFor="zip-code">Email Address</label>
             <input type="text" placeholder="10001" id="zip-code" />
 
-            <label for="city">City</label>
+            <label htmlFor="city">City</label>
             <input type="text" placeholder="New York" id="city" />
 
-            <label for="country">Country</label>
+            <label htmlFor="country">Country</label>
             <input type="text" placeholder="United States" id="country" />
           </div>
 
