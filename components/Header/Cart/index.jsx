96ed4891603bee4ab4ab7d24a3339498893c14a7
presh-031 <akingbulugbe.dev@gmail.com>
2023-05-05 18:43:32 +0100

    chore: still working on popup

--- components/Header/Cart/index.jsx
+++ components/Header/Cart/index.jsx
@@ -1,12 +1,25 @@
+import "reactjs-popup/dist/index.css";
+
 import { useDispatch, useSelector } from "react-redux";
 
 import { emptyCart } from "@/features/cart/cartSlice";
+import Image from "next/image";
+import Popup from "reactjs-popup";
+import cartIcon from "../../../assets/shared/desktop/icon-cart.svg";
 import CartItem from "./CartItem";
 
-const Cart = ({ setCartIsOpen }) => {
+const Cart = ({ close }) => {
   const cart = useSelector((state) => state.cart);
 
   const dispatch = useDispatch();
+
+  const getTotalQuantity = () => {
+    let total = 0;
+    cart.forEach((item) => {
+      total += item.quantity;
+    });
+    return total;
+  };
 
   // Logic to get total items and price in cart
   const getTotal = () => {
@@ -20,51 +33,58 @@ const Cart = ({ setCartIsOpen }) => {
   };
 
   return (
-    <>
-      <div
-        onClick={() => {
-          setCartIsOpen(false);
-        }}
-        className="modalBg absolute top-0 right-0 bottom-0  w-[100%] border border-red-800 bg-black bg-opacity-20 "
-      ></div>
-      <div className="absolute right-[25px] top-[24px] left-[25px] z-10 overflow-hidden rounded-xl bg-white px-[2.8rem] py-[3.2rem] outline">
-        <div className="mb-[3.1rem] flex justify-between">
-          <p className="text-[1.8rem] font-bold leading-[2.4590rem] tracking-[.129rem]">
-            CART <span>({getTotal().totalQuantity})</span>
-          </p>
-          <p
-            onClick={() => {
-              dispatch(emptyCart());
-              setCartIsOpen((prevCartIsOpen) => !prevCartIsOpen);
-            }}
-            className="text-[1.5rem] font-medium leading-[2.5rem] text-black opacity-50 hover:underline"
-          >
-            Remove all
+    <Popup
+      trigger={
+        <div className="relative">
+          <Image src={cartIcon} alt="cart" />
+          <p className="absolute top-4 left-[-2px] flex h-6 w-6 items-center  justify-center rounded-full bg-white text-black">
+            {getTotalQuantity() || 0}
           </p>
         </div>
-        {cart?.map((item) => (
-          <CartItem
-            key={item.id}
-            id={item.id}
-            image={item.image}
-            name={item.name}
-            price={item.price}
-            quantity={item.quantity}
-          />
-        ))}
-        <div className="mt-[3.2rem] mb-[2.4rem] flex justify-between ">
-          <p className="text-[1.5rem] font-medium leading-[2.5rem] text-black opacity-50">
-            TOTAL
-          </p>
-          <p className="text-[1.80rem] font-bold leading-[2.4rem]">
-            ${getTotal().totalPrice}
-          </p>
+      }
+      modal
+      nested
+    >
+      {(close) => (
+        <div className="w-fit rounded-xl bg-white px-[2.8rem] py-[3.2rem] outline">
+          <div className="mb-[3.1rem] flex justify-between">
+            <p className="text-[1.8rem] font-bold leading-[2.4590rem] tracking-[.129rem]">
+              CART <span>({getTotal().totalQuantity})</span>
+            </p>
+            <p
+              onClick={() => {
+                dispatch(emptyCart());
+                // setCartIsOpen((prevCartIsOpen) => !prevCartIsOpen);
+              }}
+              className="text-[1.5rem] font-medium leading-[2.5rem] text-black opacity-50 hover:underline"
+            >
+              Remove all
+            </p>
+          </div>
+          {cart?.map((item) => (
+            <CartItem
+              key={item.id}
+              id={item.id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+              quantity={item.quantity}
+            />
+          ))}
+          <div className="mt-[3.2rem] mb-[2.4rem] flex justify-between ">
+            <p className="text-[1.5rem] font-medium leading-[2.5rem] text-black opacity-50">
+              TOTAL
+            </p>
+            <p className="text-[1.80rem] font-bold leading-[2.4rem]">
+              ${getTotal().totalPrice}
+            </p>
+          </div>
+          <button className="w-full bg-[#D87D4A] bg-[] py-[1.5rem] text-[1.3rem] font-bold leading-[1.776rem] tracking-[0.1rem] text-white outline">
+            CHECKOUT
+          </button>
         </div>
-        <button className="w-full bg-[#D87D4A] bg-[] py-[1.5rem] text-[1.3rem] font-bold leading-[1.776rem] tracking-[0.1rem] text-white outline">
-          CHECKOUT
-        </button>
-      </div>
-    </>
+      )}
+    </Popup>
   );
 };
 
--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,18 +1,22 @@
 import "react-sliding-side-panel/lib/index.css";
+import "reactjs-popup/dist/index.css";
 
+import { useDispatch, useSelector } from "react-redux";
+
+import { emptyCart } from "@/features/cart/cartSlice";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import Popup from "reactjs-popup";
 import cartIcon from "../../assets/shared/desktop/icon-cart.svg";
 import logo from "../../assets/shared/desktop/logo.svg";
 import hamburgerMenu from "../../assets/shared/tablet/icon-hamburger.svg";
-import Cart from "./Cart/index";
+import CartItem from "./Cart/CartItem";
 import MenuItems from "./MenuItems";
 
 const Header = () => {
   const cart = useSelector((state) => state.cart);
 
+  const dispatch = useDispatch();
   const getTotalQuantity = () => {
     let total = 0;
     cart.forEach((item) => {
@@ -21,7 +25,18 @@ const Header = () => {
     return total;
   };
 
-  const [cartIsOpen, setCartIsOpen] = useState(false);
+  // Logic to get total items and price in cart
+  const getTotal = () => {
+    let totalQuantity = 0;
+    let totalPrice = 0;
+    cart.forEach((item) => {
+      totalQuantity += item.quantity;
+      totalPrice += item.price * item.quantity;
+    });
+    return { totalPrice, totalQuantity };
+  };
+
+  // const [cartIsOpen, setCartIsOpen] = useState(false);
   return (
     <>
       <header className="flex items-center justify-between bg-[#101010]  py-[2.2rem]  px-[2.4rem]  sm:justify-start sm:gap-[4.2rem] sm:py-[3.2rem] sm:px-[4rem] lg:justify-between lg:py-[3.5rem]  xl:px-[16.5rem] ">
@@ -42,24 +57,59 @@ const Header = () => {
           <MenuItems />
         </div>
 
-        <div
-          onClick={() => {
-            setCartIsOpen((prevCartIsOpen) => !prevCartIsOpen);
-          }}
-          className="relative"
+        <Popup
+          trigger={
+            <div className="relative">
+              <Image src={cartIcon} alt="cart" />
+              <p className="absolute top-4 left-[-2px] flex h-6 w-6 items-center  justify-center rounded-full bg-white text-black">
+                {getTotalQuantity() || 0}
+              </p>
+            </div>
+          }
+          modal
+          nested
         >
-          <Image src={cartIcon} alt="cart" />
-          <p className="absolute top-4 left-[-2px] flex h-6 w-6 items-center  justify-center rounded-full bg-white text-black">
-            {getTotalQuantity() || 0}
-          </p>
-        </div>
+          {(close) => (
+            <div className="w-fit rounded-xl bg-white px-[2.8rem] py-[3.2rem] outline">
+              <div className="mb-[3.1rem] flex justify-between outline">
+                <p className="text-[1.8rem] font-bold leading-[2.4590rem] tracking-[.129rem]">
+                  CART <span>({getTotal().totalQuantity})</span>
+                </p>
+                <p
+                  onClick={() => {
+                    dispatch(emptyCart());
+                    // setCartIsOpen((prevCartIsOpen) => !prevCartIsOpen);
+                  }}
+                  className="text-[1.5rem] font-medium leading-[2.5rem] text-black opacity-50 hover:underline"
+                >
+                  Remove all
+                </p>
+              </div>
+              {cart?.map((item) => (
+                <CartItem
+                  key={item.id}
+                  id={item.id}
+                  image={item.image}
+                  name={item.name}
+                  price={item.price}
+                  quantity={item.quantity}
+                />
+              ))}
+              <div className="mt-[3.2rem] mb-[2.4rem] flex justify-between ">
+                <p className="text-[1.5rem] font-medium leading-[2.5rem] text-black opacity-50">
+                  TOTAL
+                </p>
+                <p className="text-[1.80rem] font-bold leading-[2.4rem]">
+                  ${getTotal().totalPrice}
+                </p>
+              </div>
+              <button className="w-full bg-[#D87D4A] bg-[] py-[1.5rem] text-[1.3rem] font-bold leading-[1.776rem] tracking-[0.1rem] text-white outline">
+                CHECKOUT
+              </button>
+            </div>
+          )}
+        </Popup>
       </header>
-
-      {cartIsOpen && (
-        <div className="relative">
-          <Cart setCartIsOpen={setCartIsOpen} />
-        </div>
-      )}
     </>
   );
 };
