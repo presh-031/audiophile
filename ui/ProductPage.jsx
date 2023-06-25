aea67d87293bbb6425d656f6bdaa27505d6b9c5b
presh-031 <akingbuluge.dev@gmail.com>
2023-06-25 04:07:12 -0700

    chore: testing access

--- ui/ProductPage.jsx
+++ ui/ProductPage.jsx
@@ -1,6 +1,6 @@
 const ProductPage = ({ children }) => {
   return (
-    <div className=" px-[2.4rem]  sm:px-[4rem] xl:px-[16.5rem]">{children}</div>
+    <div className="px-[2.4rem] sm:px-[4rem] xl:px-[16.5rem]">{children}</div>
   );
 };
 
